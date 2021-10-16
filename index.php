2af9eb243a3b4015d7a6d05c6cb9a136eb8183a1
firaCS-73 <umerfiromsa@gmail.com>
2021-10-16 13:29:07 +0300

    added the slide area

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>afro_zoma</title>
+    <link href="fa/svg-with-js/css/fa-svg-with-js.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="css/style.css" rel="stylesheet"> -->
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="wrapper">
+        
+        <!-- area of the navigation -->
+        <div class="navigation-bar"> 
+            <div class="container">
+    
+                <!-- logo area -->
+                <div class="logo">
+                    <img src="images/logo.png" alt="afro_zoma_logo">
+                </div>
+                
+                <!-- the search bar -->
+                <div class="search-area">
+                    <form action="" method="post">
+                        <input type="text" name="search_box" class="search_box" placeholder="Search all items...">
+                        <button class="search_btn btn btn-success"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+                
+                <!-- user menu -->
+                <div class="user-menu">
+                    <ul>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" area-hoshpopup="true" area-expaded="false"><i class="fas fa-user"></i> My Account</a>
+                        
+                            <!-- dropdown box menu -->
+                            <div class="dropdown-menu dropdown-menu-right bg-dark" style="width:250px;">
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-user-circle"></i> &nbsp;Your Account</button></a>
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-cube"></i> &nbsp;Your Order</button></a>
+                                <a href="#"><button type="button" class="dropdown-item"><i class="fas fa-heart"></i> &nbsp;Wishlist</button></a>
+                                
+                                <div class="dropdown-divider"></div>
+                                <p class="text-center text-white" style="height:15px; line-height: 20px;"><small>if your are new user</small></p>
+                                
+                                <a href="#"><button type="button" class="dropdown-item text-center"><i class="fas fa-user"></i> &nbsp;Register</button></a>
+                                <a href="#"><button type="button" class="dropdown-item text-center bg-danger"><i class="fas fa-user"></i> &nbsp;Login</button></a>
+                        </li>
+                        
+                        <li><a href="#"><i class="fas fa-cart-arrow-down"></i> Cart</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+
+        <!-- slider area -->
+        <div class="slider-area">
+            <div class="slider">
+                <div>
+                    <a href="#"><img src="images/slider/slider1.jpg"></a>
+
+                    <!-- slider content -->
+                    <div class="slider-content">
+                        <h3 class="text-white text-capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, unde!</h3>
+                        <a href="#"><buton class="btn btn-primary"><i class="fas fa-cart-plus"></i> Buy Now</buton></a>
+                        <a href="#"><button class="btn btn-danger ml-5"><i class="fas fa-book"></i> Read More</button></a>
+                    </div>
+                </div>
+
+                <div>
+                    <a href="#"><img src="images/slider/slider2.jpg"></a>
+                </div>
+
+                <div>
+                    <a href="#"><img src="images/slider/slider3.jpg"></a>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+    
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="fa/svg-with-js/js/fontawesome-all.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
